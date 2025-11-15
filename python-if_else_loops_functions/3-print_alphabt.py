544b6bdca5b66b76ae33a6cde984abdb889f7511
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-15 21:32:41 +0000

    dasH

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-a = 96
-for i in range(26):
- a = a + 1
- if  a != 101 and a != 113:
-  print("{}".format(chr(a)),end="")
+for c in range(97, 123):
+    if c != 101 and c != 113:  
+        print("{}".format(chr(c)), end="")
