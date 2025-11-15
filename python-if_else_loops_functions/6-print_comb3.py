5e84896dd639bcbade385ae38bcc1f4de3cfaf37
root <rovsanagabeyov@gmail.com>
2025-11-15 23:51:23 +0000

    dasH

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for i in range(8):
- for  a in range(i+1,10):
-     print("{:d}{:d}, ".format(i,a),end="")
-
-print("89",end="")
+for i in range(10):
+    for j in range(i + 1, 10):
+        if i != 8 or j != 9:
+            print("{:d}{:d}, ".format(i, j), end="")
+        else:
+            print("{:d}{:d}".format(i, j))
