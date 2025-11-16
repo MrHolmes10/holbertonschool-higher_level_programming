b8f96c88fc546e3bfbcc65403453cffe6a4fea52
root <rovsanagabeyov@gmail.com>
2025-11-16 01:09:11 +0000

    dasH

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,11 +2,11 @@
 def uppercase(str):
  STR=""
  for i in str:
-    if ord("A")<=ord(i)<=ord("Z"):
-       STR=STR+i
-    elif ord("a")<=ord(i)<=ord("z"):
+    if ord("a")<=ord(i)<=ord("z"):
        t=ord(i)-(ord("a")-ord("A"))
        STR=STR+chr(t)
+    else:
+       STR=STR+i
  print(f"{STR}".format(STR))
 
 
