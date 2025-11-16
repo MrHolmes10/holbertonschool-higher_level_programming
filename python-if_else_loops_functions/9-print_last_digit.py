5c9aff1943d6c91561ee4caf731d305f6ad4104e
root <rovsanagabeyov@gmail.com>
2025-11-16 01:29:33 +0000

    dasH

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(n):
- if n>=0:
-   print(n%10)
- else:
-   print((n%10)-10) 
+  if n>=0:
+    l=n%10
+  else:
+    l=(n*(-1))%10 
+ return l
+ print(l, end="")
