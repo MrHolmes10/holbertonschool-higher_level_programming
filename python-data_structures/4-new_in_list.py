39334cc07e5da5aae0cf65c85072a956604ae644
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-17 20:54:49 +0000

    dasH

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-  newl = my_list[:]
-  if idx < 0 or idx >= len(my_list):
-    mylist = my_list
-    return mylist
-  newl[idx] = element
-  return newl
+    newl = my_list[:]
+    if idx < 0 or idx >= len(my_list):
+       mylist = my_list
+       return mylist
+    newl[idx] = element
+    return newl
