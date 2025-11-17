1097827fb6925c5f25725a31fc673b48e68a7852
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-17 19:19:43 +0000

    dasH

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,8 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
   for i in matrix :
     for j in range(len(i)) :
-      if j <len(i):
-        print("{:d}".format(j),end=" ")
+      if j <len(i)-1:
+        print("{:d}".format(i[j]),end=" ")
       else:
-        print("{:d}".format(j),end="/n" 
+        print("{:d}".format(i[j]),end="/n" 
 
