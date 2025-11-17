d52b261bfa54bd7f0bfe8d6f23db8bdac66931f8
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-17 18:23:34 +0000

    dasH

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-   print( my_list.reverse())x=
+   print( my_list.reverse())
      print(i)
  # for  in my_list.reverse():
   # print("{:d}".format(n))
