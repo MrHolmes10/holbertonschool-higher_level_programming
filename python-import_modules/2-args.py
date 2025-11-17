bc21020b2c8c8a95805e964949a5d6a3defa78da
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-17 14:52:01 +0000

    dasH

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys
-L=sys.argv[1:]
-a=1
-if len(L)==0:
- print("0 argumnets.")
-else:
- print("{} argument:".format(len(L)))
- for i  in L: 
-   print("{}: {}".format(a,i))
-   a=a+1
+if  __name__ == "__main__":
+ import sys
+ L=sys.argv[1:]
+ a=1
+ if len(L)==0:
+  print("0 argumnets.")
+ else:
+  print("{} argument:".format(len(L)))
+  for i  in L: 
+    print("{}: {}".format(a,i))
+    a=a+1
