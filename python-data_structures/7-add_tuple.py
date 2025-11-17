e0e93c8f3e1e2d658aaf0727ef74c11390dca578
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-17 19:37:01 +0000

    dasH

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+   if len(tuble_a)>0:
+     a1=tuble_a[0]
+   else:
+     a1=0
+   if len(tuble_a)>1:
+     a2=tuble_a[1]
+   else:
+     a2=0  
+   if len(tuble_b)>0:
+     b1=tuble_b[0]
+   else: 
+     b1=0
+   if len(tuble_B)>1:
+     b2=tuble_b[1]
+   else:
+     b2=0
