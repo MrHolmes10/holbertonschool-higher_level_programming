986e4113a1033d09b9e013311346a2188752819f
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-17 20:14:53 +0000

    dasH

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-  resl=my_list[:]
+  resl=my_list[]
   for i in resl:
    if i%2==0:
-     resl[i-1]=True
+     resl[i]=True
    else:
-     resl[i-1]=False
+     resl[i]=False
   return resl
