8a33827d662437fa0f365519bd6ae08470bf374a
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-17 20:03:31 +0000

    dasH

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,4 +3,8 @@ def max_integer(my_list=[]):
  if len(my_list)==0:
    return None
  else:
-   return  max(my_list)
+   big=my_list[0]
+   for i in my_list:
+      if i > big :
+        big=i 
+   return big   
