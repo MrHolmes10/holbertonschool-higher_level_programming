506cc0e065593f18d43186a3c061056054e28708
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-19 13:37:49 +0000

    ff

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,8 +3,8 @@ def search_replace(my_list, search, replace):
   a=[]
   for i in my_list:
      a.append(i)
-     
-  for x in range(1,len(a)-1):
+  return a   
+  for x in range(1,len(a)+1):
    if x==search:
      a[x-1]=replace
    else:
