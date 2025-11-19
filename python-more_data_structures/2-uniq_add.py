50227829eae1a2aa676718dd13a4dcd05bfccbf3
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-19 13:48:23 +0000

    ff

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+   a=[]
+   for i in my_list
+       if i is not in a
+          a.append(i)
+          continue
+       else:
+          continue
+       return a
