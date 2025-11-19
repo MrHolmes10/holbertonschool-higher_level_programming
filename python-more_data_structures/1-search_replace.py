35819b9bccc8572255f21a77a70b68a35f67d979
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-19 13:31:15 +0000

    ff

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+  a=[]
+  for i in my_list:
+     a.append(i)
+     
+  for x in range(len(a)):
+   if x==search:
+     a[x]=reeplace
+    
+  return a
+ 
