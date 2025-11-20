1dfb17703806ff8f8caa6e86f0b776ceaf1555d9
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-20 22:53:08 +0000

    ff

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix = []):
  new_mat = []
-    for i in matrix:
-        a = [x**2 for x in i]
-        new_mat.append(a)
-    return new_mat
+ for i in matrix:
+     a = [x**2 for x in i]
+     new_mat.append(a)
+ return new_mat
