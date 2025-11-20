f1f5643ef89f1d8b97e9f482e149e54335b8a250
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-20 21:23:38 +0000

    ff

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -4,4 +4,4 @@ def multiply_by_2(a_dictionary):
     for i in list(a_dictionary):
          mydi[i]=a_dictionary
     for t in  mydi:
-        print("{}: {}".format(t,int(mydi[t])*2))
+        print("{}: {}".format(t,srt(int(mydi[t])*2)))
