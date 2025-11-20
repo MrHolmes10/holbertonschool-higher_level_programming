6c93d93e55157d3ff43863b57a36c302e4db80a4
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-20 20:26:19 +0000

    ff

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    a = []
-    for i in a_dictionary:
-        tuple(i)
-        a.append(i)
-    a.sort()
-    for t in a:
-        print("{}: {}".format(t,a_dictionary[t]))
+      for i in sorted(a_dictionary.keys()):
+        print("{}: {}".format(i, a_dictionary[i]))
