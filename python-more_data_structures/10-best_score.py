ebd45fcac2f81ce841373a7ad69305ab4899d2c3
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-20 22:13:48 +0000

    ff

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -7,5 +7,5 @@ def best_score(a_dictionary):
        if a_dictionary[i] > a: 
           a=a_dictionary[i]
           t=i
-          keys = [key for key, val in a_dictionary.items() if val == a] 
+          keys = key for key, val in a_dictionary.items() if val == a
       return keys
