a79b70f8da91bf6629369102f5bb1f401a2f3afe
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-20 22:09:20 +0000

    ff

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary != None  :
-      a = None
+      a = 0
       for i in a_dictionary:
        t=0
        if a_dictionary[i] > a: 
