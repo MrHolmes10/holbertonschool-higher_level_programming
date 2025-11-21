f46fee467be208fac673fa92b8aedf104c44d801
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-21 13:49:56 +0000

    ff

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     a = 0
-    for i in range(x-1):
+    STR=""
+    for i in range(x): 
         try:
-            print("{}".format(my_list[i]), end = "")
+            s=str(my_list[i])
+            STR=STR+s
             a = a + 1
+            
         except:
+            print(STR,end=" ")
+            print()
             return a
+            
+    print(STR,end=" ")
+    print()
     return a
