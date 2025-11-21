cf53447d3cc379d5b4adecaf5eb7499c1761230c
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-21 14:14:13 +0000

    ff

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,6 +2,8 @@
 def safe_print_division(a, b):
     try:
         result = a / b
+        print("Inside result: {}".format(result))
         return result
     except:
+        print("Inside result: None")
         return None
