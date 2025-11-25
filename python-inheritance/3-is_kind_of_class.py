037b29e3df56f3ef3f808a4f05cda0f119c0df89
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-25 11:00:05 +0000

    ff

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -3,8 +3,7 @@
 def is_kind_of_class(obj, a_class):
     "'asdf'"
     if isinstance(obj, a_class):
-       retrun True
-    retrun False
+       return True
+    return False
 
->>> if is_kind_of_class(a, int):
->>>    print("{} comes from {}".format(a, int.__name__))
+
