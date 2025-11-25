4b160f9bce62654655baba873e6246af492d99ad
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-25 22:05:41 +0400

    Update 6-base_geometry.py

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 "'bayadir yuxusuzam'"
+
+
 class BaseGeometry():
-     "'sleep i want it that wayyy'"
-     def area(self):
-         "'zafer alla gozz'"
-         raise Exception("area() is not implemented")
+    """sleep i want it that wayyy"""
+     
+    def area(self):
+        """zafer alla gozz"""
+        raise Exception("area() is not implemented")
