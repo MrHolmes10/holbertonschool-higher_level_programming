f60a0b55ae30e3f7b0bfcbb5f9c42ba544dd417f
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-25 11:51:11 +0000

    ff

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 "123456789''"
-class Rectangle():
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+class Rectangle(BaseGeometry):
+     
       "'jhgfd'"
      def __init__(self, width, height):
 	 "'1234567'"
