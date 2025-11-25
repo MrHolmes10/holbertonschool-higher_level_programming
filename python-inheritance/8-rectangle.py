c2e3b60ca1ec051de5741a93852a95f50f139862
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-25 11:48:16 +0000

    ff

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"123456789''"
+class Rectangle():
+      "'jhgfd'"
+     def __init__(self, width, height):
+	 "'1234567'"
+         self.integer_validator("width", width)
+	 self.__width = width
+         self.integer_validator("height", height)
+         self.height = height
+     
