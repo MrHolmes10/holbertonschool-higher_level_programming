c77bb3e26276f177b62065a513c448f519750b61
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-25 09:56:56 +0000

    ff

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,6 +4,4 @@ class MyList(list):
     "'qwertyuiopasdfg'"
     def print_sorted(self):
         "'999'"
-        l_sorted = self.copy()
-        l_sorted.sort()
-        print(l_sorted)
+        print(sorted(self))
