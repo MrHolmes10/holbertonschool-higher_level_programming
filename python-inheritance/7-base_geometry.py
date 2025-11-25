e913e18c1fdaf56a638ab9656c71030ecccb4af9
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-25 19:53:36 +0400

    Update 7-base_geometry.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -2,14 +2,19 @@
 """7777"""
 
 class BaseGometry():
-     """77445"""
-     
+    """i am the best"""
+    
+    
     def area(self):
          """fff"""
+    
          raise Exception("area() is not implemented")
      
+    
+    
     def integer_validator(self, name, value):
          """rtrt"""
+    
          if type(value) != int :
             raise TypeError("{} must be an integer".format(name))
          if value <= 0 :
