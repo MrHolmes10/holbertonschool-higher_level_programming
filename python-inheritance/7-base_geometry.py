ba332a196841bb9427508e9fc2a4a5478ca7af04
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-25 11:33:50 +0000

    ff

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"'7777'"
+class BaseGometry():
+      "'77445'"
+     def area(self):
+         "'fff'"
+         raise Exception("area() is not implemented")
+     def integer_validator(self, name, value):
+         "'rtrt'"
+         if type(value) != int :
+            raise TypeError("{} must be an integer".format(name))
+         if value <= 0 :
+            raise ValueError("")
