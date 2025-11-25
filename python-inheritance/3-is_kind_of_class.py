d9ed016f539a84eeadff76194eb3f224ff203385
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-25 10:57:01 +0000

    ff

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -5,3 +5,6 @@ def is_kind_of_class(obj, a_class):
     if isinstance(obj, a_class):
        retrun True
     retrun False
+
+>>> if is_kind_of_class(a, int):
+>>>    print("{} comes from {}".format(a, int.__name__))
