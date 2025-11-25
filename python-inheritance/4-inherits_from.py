addec7e963e10e56a19cae859048e1cb8c85e52e
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-25 11:11:21 +0000

    ff

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,4 +1,6 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 "'gfgfd'"
 def inherits_from(obj, a_class):
-    if 
+    if issubclass(type(obj), a_class) and type(obj0 != a_class:
+       return True 
+    return False
