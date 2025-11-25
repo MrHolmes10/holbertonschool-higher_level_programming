821f37f23d575f3359f66077f6fdae646b8bb463
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-25 22:08:59 +0400

    Update 9-rectangle.py

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-"""7777"""
+"""BaseGeometry-dən miras alınır"""
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-class Rectangle:
-    """123456789"""
+
+class Rectangle(BaseGeometry):
+    """Düzbucaqlı sinfi, BaseGeometry-dən miras alır"""
 
     def __init__(self, width, height):
+        """Düzbucaqlını width və height ilə yaradır"""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
     def area(self):
-        """qwsdfghbn"""
+        """Düzbucaqlının sahəsini qaytarır"""
         return self.__width * self.__height
 
-    
     def __str__(self):
-        return "[Rectangle] <{}>/<{}>".format(self.__width, self.__height)
-    
+        """Düzbucaqlını istədiyimiz formatda string-ə çevirir"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
