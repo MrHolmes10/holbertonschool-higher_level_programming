9a590ecceadf5d61fc92aba0e5a01a4a6c83a9eb
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-27 17:36:33 +0000

    ff

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""123456"""
+import os, sys
+load_from_json_file = __import__(''6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+filename = "add_item.json"
+if os.path.exists(filename):
+    items = load_from_json_file(filename)
+else:
+    items = []
+
+items.extend(sys.argv[1:])
+
+save_to_json_file(items, filename)
