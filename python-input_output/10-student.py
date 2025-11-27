52fd1b88fd9400e24704923675fbb1f1ce513c3c
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-27 18:15:43 +0000

    ff

---
+++ python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""tasks 10"""
+
+class Student:
+     """ class"""
+     def __init__(self, first_name, last_name, age):
+         """you tube"""
+
+         self.first_name = first_name
+         self.last_name = last_name
+         self.age = age
+
+    def to_json(self, attrs=None):
+        """if att """
+
+         if (isinstance(attrs, list) and
+                all(isinstance(x, str) for x in attrs)):
+            result = {}
+            for key in attrs:
+                if hasattr(self, key):
+                    result[key] = getattr(self, key)
+            return result
+
+        return self.__dict__
