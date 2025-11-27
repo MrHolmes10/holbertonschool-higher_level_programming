9494cf110d29ba35c98bc2f4e9bef253db81069d
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-27 17:00:50 +0000

    ff

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -3,7 +3,8 @@
 
 import json
 
+
 def load_from_json_file(filename):
     """!@#$%^&*"""
-   with open(filename, "r") as oo:
-        return json.load(oo)
+    with open(filename, 'r') as oo:
+         return json.load(oo)
