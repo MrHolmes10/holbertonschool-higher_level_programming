50eee085a2cd0950fd01750e308c7e2edcc82ae2
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-27 15:20:12 +0000

    ff

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""2164178723yr3ruiy9thwet9ppw794"""
 def read_file(filename=""):
+    """1234567890-asdfghjklertyu"""
     with open(filename, encoding="utf-8") as oo:
         print(oo.read())
+
