06aa47141b80ae7348c90875e68cb0fb9dfdb16c
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-28 15:55:12 +0400

    Create 11-student.py

---
+++ python-input_output/11-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""tasks 11"""
+import json as js
+
+
+class Student:
+    """ class"""
+    def __init__(self, first_name, last_name, age):
+         """you tube"""
+
+         self.first_name = first_name
+         self.last_name = last_name
+         self.age = age
+
+    def to_json(self, attrs=None):
+         """if att """
+
+         if (isinstance(attrs, list) and
+                all(isinstance(x, str) for x in attrs)):
+            result = {}
+            for key in attrs:
+                if hasattr(self, key):
+                    result[key] = getattr(self, key)
+            return result
+
+         return self.__dict__
+   
+    def reload_from_json(self, json):
+        """123456789"""
+        a = js.loads(json)
+        for i,j in a.items() :
+            self.i = j
