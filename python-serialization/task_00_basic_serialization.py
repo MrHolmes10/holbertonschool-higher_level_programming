d2cb9ec1afe4f4b9400eb8fe06b6825c582e2dae
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-28 15:57:10 +0000

    ff

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" 7777998888 """
+
+import json
+
+def serialize_and_save_to_file(data, filename):
+    """1133"""
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+def load_and_deserialize(filename):
+    """ ggg"""
+    with open(filename, 'r', encoding='utf-8') as f:
+         return json.load(f)
