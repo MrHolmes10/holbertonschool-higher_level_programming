62ffedd373fb65a6fce36c1fdb03d496872cd39d
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-28 17:57:06 +0400

    Update 12-pascal_triangle.py

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python3
 """tasks 12"""
 
-def pascal_triangle(n):
-    """ easy idi """
-    L=[[1],[1,1]]
-    for x in range(3,n+1):
-        T=[1]*x
-        for i in range(len(T)):
-            try:
-                l=L[x-2]
-                 
-                T[i+1]=l[i]+l[i+1]
+def pascal_triangel(n):
+    """easy !!"""
+    try:
+        if n <= 0:
+            return 0
+        L=[[1],[1,1]]
+        for x in range(3,n+1):
+            T=[1]*x
+            for i in range(len(T)):
+                try:
+                    l=L[x-2]
+                    
+                    T[i+1]=l[i]+l[i+1]
+                    
                 
-            
-                continue
-            except:
-                try: 
-                    T[i]=l[i]+l[i-1]
                     continue
                 except:
-                    T[i]=1
-                 
-              
-            L.append(T)       
-    for lits in L:
-        print(lits)  
+                    try: 
+                        T[i]=l[i]+l[i-1]
+                        continue
+                    except:
+                        T[i]=1
+                    
+                
+                L.append(T)       
+        for lits in L:
+            print(lits)           
+    except:
+        return 0
