abfe8e1a9ab215f942f5e24703eba13ace74123a
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-28 17:24:57 +0400

    Create  12-pascal_triangle.py

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""tasks 12"""
+
+def pascal_triangel(n):
+    L=[[1],[1,1]]
+    for x in range(3,n+1):
+        T=[1]*x
+        for i in range(len(T)):
+            try:
+                l=L[x-2]
+                 
+                T[i+1]=l[i]+l[i+1]
+                
+            
+                continue
+            except:
+                try: 
+                    T[i]=l[i]+l[i-1]
+                    continue
+                except:
+                    T[i]=1
+                 
+              
+            L.append(T)       
+    for lits in L:
+        print(lits)  
