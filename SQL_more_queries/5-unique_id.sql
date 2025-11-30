4eebd477eeeb543f275a85c53d6e88141e31ccd6
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-30 15:30:23 +0000

    ff

---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,6 @@
+-- Database
+-- create table unique
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
+);
---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,18 @@
+-- Database
+-- select
+SELECT
+    id,
+    name
+FROM
+    cities
+WHERE
+    state_id IN (
+        SELECT
+            id
+        FROM
+            states
+        WHERE
+            name = 'California'
+        )
+ORDER BY
+    id ASC;
