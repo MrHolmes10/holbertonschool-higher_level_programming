5be5f8a452d933dcc633a4dff0551d927ba74778
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-11-30 14:25:30 +0000

    ff

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,8 @@
 -- Database
-CREATE TABLE if not exists second_table
-CREATE TABLE second_table(
-
+CREATE TABLE if not exists second_table(
+id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(256)
+score INT
 );
+
+INSERT INTO secod_table(id,name,score) VALUES (1,"John",10),(2,"Alex",3),(3,"Bob",14),(4,"George",8);
