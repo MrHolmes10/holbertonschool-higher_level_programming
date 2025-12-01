48b1dcd46802ea7cc9b15fa9ee510120587c7ca7
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-12-01 13:44:33 +0000

    ff

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """qaqa bu kimdi """
 if  __name__ == "__main__":
-    import urllib.request , sys
+    import urllib.request
+    import  sys
     url = sys.argv[1]
     with urllib.request.urlopen(url) as urll:
          headers = response.info()
