88be7c72beb5f6601b23e6e7f837dc65cb302f78
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-12-01 14:10:43 +0000

    ff

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -8,11 +8,13 @@ import urllib.parse
 if _name__ == "__main__":
    url = sys.argv[1]
    email = sys.argv[2]
-  
+   
+   # post  
    data = urllib.parse.urlencode({"email": email}).encode("utf-8")
-
+   
+   # post req
    with urllib.request.urlopen(url, data) as urll:
         bod = urll.read()
-   
+   # decode
    print(body.decode("utf-8"))
 
