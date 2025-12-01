78fa3b99ff0e30f8b3a8ebfd81c4f82bd2320834
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-12-01 13:25:57 +0000

    ff

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if  __name__ == "__main__":
+    import urllib.request
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as res:
+          res = res.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(res)))
+    print("\t- content: {}".format(res))
+    print("\t- utf8 content: {}".format(res.decode("utf-8")))
