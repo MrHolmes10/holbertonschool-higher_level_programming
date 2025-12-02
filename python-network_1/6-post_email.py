71147e6d62b8a8375915e45854dc5790187ab70c
MrHolmes10 <rovsanagbeyov@gmail.com>
2025-12-02 15:47:52 +0000

    ff

---
+++ python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" task 7"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    emial = sys.argv[2]
+    
+    data = {"email": email}
+    fako = requests.post(url, data=data)
+    print(faiq.text)
