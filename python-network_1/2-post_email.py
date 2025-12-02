5ab9f7771801944020c11f2f08857de31403b619
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-12-02 18:32:31 +0400

    Update 2-post_email.py

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -16,5 +16,5 @@ if __name__ == "__main__":
    with urllib.request.urlopen(url, data) as urll:
         bod = urll.read()
    # decode
-   print(body.decode("utf-8"))
+   print(bod.decode("utf-8"))
 
