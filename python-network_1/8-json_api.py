7f32d749d37235c13e0ee9d02b367cc6265d3391
MrHolmes10 <rovsanagbeyov@gmail.com>
2025-12-02 16:24:23 +0000

    ff

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-""" tasks json"""
+"""Sends a POST request to search_user and prints the result"""
 
 import requests
 import sys
+
 if __name__ == "__main__":
-    if len( sys.argv[1])>0:
-       q = sys.argv[1]
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
     url = "http://0.0.0.0:5000/search_user"
     data = {"q": q}
-    
-    
-   try:
-       faiq = requests.post(url, data=data)
-       faiqjson = faiq.json()
-       
-       if faiqjson :
-           print("[{}] {}".format(faiqjson.get("id"), faiqjson.get("name")))
-       else:
-           print("No result")
-   except ValueError:
-       print("Not a valid JSON")
+
+    try:
+        response = requests.post(url, data=data)
+        json_response = response.json()
+
+        if json_response:
+            print("[{}] {}".format(
+                json_response.get("id"), json_response.get("name")
+            ))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
