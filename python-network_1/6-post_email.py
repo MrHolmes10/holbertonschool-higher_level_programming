c3844a2c83c9a73f8c536af49fc0906acd1cc45e
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-12-03 14:31:37 +0000

    ff

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    
+
     data = {"email": email}
     fako = requests.post(url, data=data)
     print(fako.text)
