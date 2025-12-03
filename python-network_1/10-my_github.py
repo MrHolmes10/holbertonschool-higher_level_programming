bfd9d54a8be928ea793dbf9a11a04cb4fd3256a0
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-12-03 14:19:44 +0000

    ff

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """ git hub """
 
-if  __name__ == "__main__":
+if __name__ == "__main__":
     import sys
     import requests
-    username =  sys.argv[1]
+    username = sys.argv[1]
     passwd = sys.argv[2]
-    
+
     url = "https://api.github.com/user"
-    
+
     ans = requests.get(url, auth=(username, passwd))
-  
+
     try:
         data = ans.json()
         print(data.get("id"))
