47586272ee768c944e3bc2bcb0e2f310ff517469
MrHolmes10 <rovsanagabeyov@gmail.com>
2025-12-03 14:28:04 +0000

    ff

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -5,10 +5,10 @@ import requests
 import sys
 
 if __name__ == "__main__":
-   url = sys.argv[1]
+    url = sys.argv[1]
 
-   response = requests.get(url)
+    response = requests.get(url)
    if response.status_code >= 400:
        print("Error code: {}".format(response.status_code))
    else:
-       print(response.text)
+        print(response.text)
